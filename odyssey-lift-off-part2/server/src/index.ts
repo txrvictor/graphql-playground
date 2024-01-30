a274227a5bf84203ab5183eb8c9c4e541a46c4ec
Victor Teixeira <teixeira.victorv@gmail.com>
2024-01-30 18:08:06 +0900

    Tie the resolvers, schema and RESTDataSource class together in the server's config

--- odyssey-lift-off-part2/server/src/index.ts
+++ odyssey-lift-off-part2/server/src/index.ts
@@ -1,38 +1,30 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+import { resolvers } from "./resolvers";
+import { TrackAPI } from "./datasources/track-api";
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks,
-    }),
+    typeDefs,
+    resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    // note the "async"
+    context: async () => {
+      // to enable caching to the RESTDataSource class:
+      const {cache} = server;
+
+      // this object becomes our resolver's "contextValue" argument
+      return {
+        // enable "dataSources.trackAPI" in our contextValue
+        dataSources: {
+          trackAPI: new TrackAPI({cache}),
+        },
+      };
+    },
+  });
+
   console.log(`
     🚀  Server is running!
     📭  Query at ${url}
