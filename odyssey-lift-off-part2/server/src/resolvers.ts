10ce674d9f71826672b370fac543f396b2094712
Victor Teixeira <teixeira.victorv@gmail.com>
2024-01-30 17:59:51 +0900

    Adding resolvers

---
+++ odyssey-lift-off-part2/server/src/resolvers.ts
@@ -0,0 +1,23 @@
+
+export const resolvers = {
+  Query: {
+    // returns an array of Tracks
+    tracksForHome: (parent, args, contextValue, info) => {
+      console.log("Query-tracksForHome", {parent, args, contextValue, info});
+
+      const {dataSources} = contextValue;
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  // will be used when "parent" tracksForHome is called and tries to
+  // populate "Track->author" field
+  Track: {
+    author: (parent, args, contextValue, info) => {
+      console.log("Track-author", {parent, args, contextValue, info});
+
+      const {dataSources} = contextValue;
+      const {authorId} = parent;
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+  },
+};
